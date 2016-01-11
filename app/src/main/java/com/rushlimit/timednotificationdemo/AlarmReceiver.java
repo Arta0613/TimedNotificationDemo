66451e5b220c4f66e7658fa486d63bfcec4ce166
Mohammed Amin <arta0613@gmail.com>
2016-01-10 19:36:10 -0500

    first commit

---
+++ app/src/main/java/com/rushlimit/timednotificationdemo/AlarmReceiver.java
@@ -0,0 +1,45 @@
+package com.rushlimit.timednotificationdemo;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+/**
+ * Created by Arman on 1/7/16.
+ */
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction() != null && intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            Intent myintent = new Intent(context, AlarmReceiver.class);
+            myintent.putExtra("RICHARD", "Notif");
+            myintent.putExtra("GROUP", "SURVEY");
+            PendingIntent alarmIntent = PendingIntent.getBroadcast(context, (int) System.currentTimeMillis(), myintent, 0);
+            // set for 30 seconds later
+            alarmMgr.set(AlarmManager.RTC, Long.parseLong(context.getSharedPreferences("MAIN", Context.MODE_PRIVATE).getString("TIME", "0")), alarmIntent);
+        } else {
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            Notification notification = new NotificationCompat.Builder(context)
+                    .setContentTitle("Survey Available")
+                    .setContentText(intent.getStringExtra("RICHARD"))
+                    .setSmallIcon(R.drawable.abc_ic_go_search_api_mtrl_alpha)
+                    .setContentIntent(PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0))
+                    .setAutoCancel(true)
+                    .setGroup(intent.getStringExtra("SURVEY"))
+                    .build();
+
+            notificationManager.notify(0, notification);
+        }
+    }
+}
---
+++ app/src/main/java/com/rushlimit/timednotificationdemo/MainActivity.java
@@ -0,0 +1,69 @@
+package com.rushlimit.timednotificationdemo;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlarmManager alarmMgr = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                Intent intent = new Intent(MainActivity.this, AlarmReceiver.class);
+                intent.putExtra("RICHARD", "Notif");
+                intent.putExtra("GROUP", "SURVEY");
+                PendingIntent alarmIntent = PendingIntent.getBroadcast(MainActivity.this, (int) System.currentTimeMillis(), intent, 0);
+                // set for 30 seconds later
+                alarmMgr.set(AlarmManager.RTC, Calendar.getInstance().getTimeInMillis() + 240000, alarmIntent);
+                getSharedPreferences("MAIN", MODE_PRIVATE).edit().putString("TIME", (Calendar.getInstance().getTimeInMillis() + 240000) + "").commit();
+
+                Snackbar.make(view, "Notif", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
